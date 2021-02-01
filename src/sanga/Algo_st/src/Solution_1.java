4c835903ac7892d2a08d856d81eb7aa2106bba9b
Sangah Cho <sanga12500@gmail.com>
2021-02-01 10:37:44 +0900

    프로그래머스_1-두개의 수 뽑아서 더하기

--- src/sanga/Algo_st/src/Solution_1.java
+++ src/sanga/Algo_st/src/Solution_1.java
@@ -1,11 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class Main {
-	public static void main(String[] agrs) {
-		int[] numbers = {5,0,2,7};
+/**
+ * 프로그래머스 - 두개 뽑아서 더하기
+ * @author sanga
+ *
+ */
+public class Solution_1 {
+	public int[] solution(int[] numbers) {
+
+		//int[] numbers = {5,0,2,7};
 		int[] answer = {};
 		List<Integer> addNum = new ArrayList<Integer>();
-		//오름차순으로 정렬
+		//오름차순으로 정렬 - 배열 정렬 방법
 		Arrays.sort(numbers);
 		
 		for( int i = 0; i < numbers.length; i++ ) {
@@ -16,7 +25,7 @@ public class Main {
 				}
 			}
 		}
-		
+		//오름차순으로 정렬 - List 정렬 방법
 		Collections.sort(addNum);
 		
 		answer = new int[addNum.size()];
@@ -24,6 +33,8 @@ public class Main {
 			System.out.print(i);
 			answer[i] = addNum.get(i);
 		}
-		System.out.print(answer.toString());
+		
+		return answer;
+	
 	}
 }
